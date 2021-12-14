c582bd1eba59c02993395bf074fa020c34b2bd90
SangYuhiter <sangyu.code@gmail.com>
2021-12-15 00:27:11 +0800

    5_PrintStruct fix complex

--- C++/5_PrintStruct/PrintStruct.cpp
+++ C++/5_PrintStruct/PrintStruct.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <cstring>
 #include "PrintStruct.h"
 
 int main()
 {
-    std::cout<<"print struct!"<<std::endl;
+    int a,b;
+    std::cout<<"Entry two num:";
+    std::cin>>a>>b;
+    COMPLEX r(a,b);
+    r.Print_Complex();
+    std::cout<<"print Complex struct!"<<std::endl;
+
+    {
+        static const unsigned int result_num = 100;
+	static const unsigned int temp_num = 10;
+	static const unsigned int line_count = 4;
+        char result[result_num];
+	memset(result,0,result_num);
+	char temp[temp_num];
+	memset(temp,0,temp_num);
+
+	unsigned char* puchar = (unsigned char*)&r;
+	for(size_t i = 0;i < sizeof(COMPLEX);i++)
+	{
+	    if((i+1)%line_count == 0 && i != 0)
+	    {
+	        sprintf(temp,"%02x\n",*(puchar+i));
+	    }
+	    else
+	    {
+	        sprintf(temp,"%02x ",*(puchar+i));
+	    }
+	    strcat(result,temp);
+	}
+	printf("reuslt:\n%s\n",result);
+    }
     return 0;
 }
--- C++/5_PrintStruct/PrintStruct.h
+++ C++/5_PrintStruct/PrintStruct.h
@@ -1,4 +1,18 @@
 #ifndef _PRINTSTRUCT_HEAD_
 #define _PRINTSTRUCT_HEAD_
 
+#include <iostream>
+
+typedef struct Complex
+{
+    int real_num;
+    int virtual_num;
+    Complex():real_num(0),virtual_num(0){};
+    Complex(int a,int b):real_num(a),virtual_num(b){};
+    void Print_Complex()
+    {
+        std::cout<<real_num<<"+"<<virtual_num<<"i"<<std::endl;
+    };
+}COMPLEX;
+
 #endif
