03d6bca76185cfaad93f2f5c6951f82c12876d55
SangYuhiter <sangyu.code@gmail.com>
2021-12-14 23:57:29 +0800

    5_PrintStruct

---
+++ C++/5_PrintStruct/PrintStruct.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "PrintStruct.h"
+
+int main()
+{
+    std::cout<<"print struct!"<<std::endl;
+    return 0;
+}
---
+++ C++/5_PrintStruct/PrintStruct.h
@@ -0,0 +1,4 @@
+#ifndef _PRINTSTRUCT_HEAD_
+#define _PRINTSTRUCT_HEAD_
+
+#endif
