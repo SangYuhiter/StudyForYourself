de52d99b323d3fdc6ecbbe89867ff508e2aca3d8
SangYuhiter <sangyu.code@gmail.com>
2021-12-17 01:55:28 +0800

    Python 0_HelloWorld

---
+++ Python/0_HelloWorld/HelloWorld.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+"""
+# File Name    : HelloWorld.py
+# Author       : SangYu
+# Email        : sangyu.code@gmail.com
+# Created Time : 2021年12月17日 星期五 01时46分23秒
+# Description  : 
+"""
+DEFAULT_NAME = "SangYu"
+
+def SayHello(name = DEFAULT_NAME):
+    print("Hello World",name,"!")
+
+if __name__ == "__main__":
+    SayHello()
+    pass
+
