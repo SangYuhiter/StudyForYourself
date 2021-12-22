586892b86622904288e9b201787b5a29601594a5
SangYuhiter <sangyu.code@gmail.com>
2021-12-23 00:43:58 +0800

    Stack basic Push and Pop

--- DataStructure/1_Stack/Stack.h
+++ DataStructure/1_Stack/Stack.h
@@ -5,49 +5,30 @@
 
 typedef struct Node{
     int data;
-    Node* priv;
     Node* next;
 }NODE;
 
-class LinkList{
+class Stack{
 public:
-    //default init the LinkList as dubble link
-    LinkList(){
-        head = new NODE();
-        tail = new NODE();
-        head->data = tail->data = 0;
-        head->priv = head->next = tail;
-        tail->priv = tail->next = head;
-        count = 0;
+    Stack(){
+        top = new Node();
+        top->data = -1;
+        top->next = nullptr;
     };
-    ~LinkList(){
-        NODE* temp1 = head;
-        NODE* temp2 = head;
-        while(temp1 != nullptr){
-            if(temp1 == tail){
-                temp1->next = nullptr;
-            }
-            temp1 = temp1->next;
-            std::cout<<"delete "<<temp2->data<<",count:"<<count<<std::endl;
-            delete temp2;
-            temp2 = temp1;
-            if(temp1 != head && temp1 != tail){
-                count--;
-            }
+    ~Stack(){
+        while(top != nullptr){
+            NODE* temp = top->next;
+            std::cout<<"delete "<<top->data<<std::endl;
+            delete top;
+            std::cout<<"delete end"<<std::endl;
+            top = temp;
         }
-    }
-    NODE* GetHead(){
-        return head;
     };
-    NODE* GetTail(){
-        return tail;
-    };
-    void AddNode(int data);
-    void DeleteNode(int data);
-    void PrintLinkList();
+    void Push(int data);
+    int Pop();
+    void PrintStack();
 private:
-    NODE* head;
-    NODE* tail;
+    NODE* top;
     unsigned int count;
 };
 #endif
