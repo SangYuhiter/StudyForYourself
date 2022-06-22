3be32082605712fff85bd1bdf341602940e949e8
SangYuhiter <sangyu.code@gmail.com>
2022-06-22 17:23:30 +0800

    fix libcameraservice error

--- Linux/cctagsfiles.sh
+++ Linux/cctagsfiles.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 find ./frameworks/av/camera/ -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "*.cc"
 find ./frameworks/av/include/camera/ -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "*.cc"
-find ./frameworks/av/service/camera/libcameraservice/ -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "*.cc"
+find ./frameworks/av/services/camera/libcameraservice/ -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "*.cc"
